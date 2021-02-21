a8cadfc29c5c44a6c8b93f2ab18be89901ae0510
Marco Lopez <lopez.marcohdez@gmail.com>
2021-02-20 22:12:03 -0500

    message

--- db.js
+++ db.js
@@ -1,6 +1,9 @@
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
+    ssl: {
+        rejectUnauthorized: false
+      }
 })
 
 sequelize.authenticate().then(
