0fe6810d51fc51307dd298805b4e1e46c88e3601
Marco Lopez <lopez.marcohdez@gmail.com>
2021-02-23 11:40:22 -0500

    update DB file

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize(`${process.env.DATABASE_URI}?sslmode=require`, {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
     dialectOptions: {
         ssl: {
